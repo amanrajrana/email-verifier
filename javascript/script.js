726bf26ece95895fea41e365f0cb727c34e51205
Aman Raj Rana <75106349+amanrajrana@users.noreply.github.com>
2023-07-05 12:55:16 +0530

    Refactor: Optimize code and improve documentation

--- javascript/script.js
+++ javascript/script.js
@@ -1,112 +1,109 @@
 /**
- * 
- * @param email - a string i.e email
- * @returns true or false on basis of that string includes '@' or not
+ * Checks if a string includes the '@' symbol.
+ *
+ * @param {string} email - The string to check.
+ * @returns {boolean} - True if the string includes '@', false otherwise.
  */
 const checkAtTheRate = (email) => {
     return email.includes("@");
 }
 
-
 /**
- * @param str - a string.
- * 
- * The function checks if a given string is a valid email address based on certain rules.
- * rules
- * 1. string can start and with alphaNumeric (as variable alphaNumericRegex)
- * 2. rest string only can contain alphaNumeric or three special char (- . _) [as variable alphaNumericSpecialRegex]
- * 
- * @returns an object with two key isValid an message based on certain rules
+ * Checks if a given string is a valid email address based on certain rules.
+ *
+ * @param {string} str - The string to validate.
+ * @returns {Object} - An object with two keys: isValid (boolean) and message (string) based on the validation rules.
  */
-
 function validSyntax(str) {
-
     const alphaNumericRegex = /^[A-Za-z0-9]+$/;
     const alphaNumericSpecialRegex = /^[A-Za-z0-9-._]+$/;
 
-    // string must be start with alphaNumeric latter
+    // Check if the string starts with an alphanumeric character
     if (!alphaNumericRegex.test(str.charAt(0))) {
         return {
             isValid: false,
-            message: `User Name can not start with "${str.charAt(0)}"`
-        }
+            message: `User Name cannot start with "${str.charAt(0)}"`
+        };
     }
 
-    // string must be end with alphaNumeric latter
+    // Check if the string ends with an alphanumeric character
     if (!alphaNumericRegex.test(str.charAt(str.length - 1))) {
         return {
             isValid: false,
-            message: `Domain Name can not end with "${str.charAt(str.length - 1)}"`
-        }
+            message: `Domain Name cannot end with "${str.charAt(str.length - 1)}"`
+        };
     }
 
-
-    // rest latter on can be alphaNumeric and three special char (-_.)
+    // Check if the rest of the string contains only alphanumeric or specific special characters
     if (alphaNumericSpecialRegex.test(str)) {
         return {
-            isValid: true,
-        }
+            isValid: true
+        };
     }
 
-    // default return
+    // Default return
     return {
         isValid: false,
         message: "Not a valid email"
-    }
+    };
 }
 
+/**
+ * Validates the given email address.
+ *
+ * @param {string} email - The email address to validate.
+ * @returns {Object} - An object with two keys: isValid (boolean) and message (string) indicating the validation result.
+ */
 const checkValidEmail = (email) => {
     email = email.trim();
-    let flag = false;
 
-    // Check Email must include @
+    // Check if email contains '@'
     if (!checkAtTheRate(email)) {
         return {
             isValid: false,
             message: `Email must contain '@'`
-        }
+        };
     }
 
     const indexOfAt = email.indexOf("@");
-    const userName = email.slice(0, indexOfAt);  // slice username
-    const serverName = email.slice((indexOfAt + 1)); // slice domain
+    const userName = email.slice(0, indexOfAt);  // Slice username
+    const serverName = email.slice(indexOfAt + 1); // Slice domain
 
-    // if userName and serverName with function validSyntax() if it return two value {isValid: , message} it mean userName is in valid syntax
-
+    // Validate the username and server name using the validSyntax() function
     const validUserNameSyntax = validSyntax(userName);
     const validDomainNameSyntax = validSyntax(serverName);
 
     if (!validUserNameSyntax.isValid) {
-        return validUserNameSyntax
+        return validUserNameSyntax;
     }
 
-    if (!(validDomainNameSyntax.isValid)) {
+    if (!validDomainNameSyntax.isValid) {
         return validDomainNameSyntax;
     }
 
     return {
         isValid: true,
         message: 'Valid E-mail address'
-    }
+    };
 }
 
-
-function main () {
+/**
+ * Handles the email validation and displays the result on the web page.
+ */
+function main() {
     const email = document.getElementById('userEmail').value;
-    const resultElement = document.getElementById('result')
+    const resultElement = document.getElementById('result');
 
     const isValidEmail = checkValidEmail(email);
 
-    if(isValidEmail.isValid) {
-        resultElement.style.color = 'green';
-        resultElement.innerHTML = isValidEmail.message;
-    }
-    else {
-        resultElement.style.color = 'rgba(192, 0, 0, 0.8)';
-        resultElement.innerHTML = isValidEmail.message;
-    }
+    resultElement.style.color = isValidEmail.isValid ? 'green' : 'rgba(192, 0, 0, 0.8)';
+    resultElement.innerHTML = isValidEmail.message;
 }
 
+/**
+ * Clears the validation result message.
+ */
 const clearMessage = () => {
     document.getElementById('result').innerHTML = "";
-}
+            }
+            
